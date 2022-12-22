dad943fd81ebd3cf389de0e19ee50df9e7e69702
Prabhudutta Panigrahi <58845544+p-panigrahi@users.noreply.github.com>
2022-12-22 17:32:38 +0530

    1push

---
+++ To_Find_The_Sum_of_average_of_Three_Numbers.py
@@ -0,0 +1,8 @@
+# write a program to print the sum of average of three number
+num1 = int(input("Enter any number"))
+num2 = int(input("Enter any number"))
+num3 = int(input("Enter any number"))
+sum = num1 + num2 + num3
+avg = sum / 3
+print(f"The sum of 3 number {sum}")
+print(f"The avg of 3 number {avg}")
